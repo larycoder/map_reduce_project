2994eb58651083e24a3eb8c97bd290ab07496c61
larycoder <lenhuchuhiep99@gmail.com>
2022-03-30 13:40:50 +0700

    LH    : build Daemon server to upload and download file

---
+++ src/main/java/usth/master/map/Worker.java
@@ -0,0 +1,35 @@
+package usth.master.map;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Run on slave node to execute Map on the node.
+ *
+ * 1. Split connect (TCP) to Daemon in order to upload a block of data which
+ *    then be stored in file.
+ * 2. Launch invoke (RMI) a method call() of Daemon in order to execute a Map
+ *    on the node.
+ * 3. Launch connect (TCP) to Daemon in order to download a block of data
+ *    (result).
+ * */
+public interface Worker extends Remote {
+    /**
+     * Upload data to server.
+     * @param file upload file name.
+     * @return socket port.
+     * */
+    int upload(String file) throws RemoteException;
+
+    /**
+     * Upload data to server.
+     * @param file upload file name.
+     * @return socket port.
+     * */
+    int download(String file) throws RemoteException;
+
+    /**
+     * Call map execution.
+     * */
+    void call() throws RemoteException;
+}
