cba063c9973d3f674ed33598b1ec93d84aa34413
larycoder <lenhuchuhiep99@gmail.com>
2022-04-04 09:52:50 +0700

    LH    : develop Lauch main process path

--- src/main/java/usth/master/origin/Launch.java
+++ src/main/java/usth/master/origin/Launch.java
@@ -3,10 +3,16 @@ package usth.master.origin;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.rmi.Naming;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import usth.master.app.MapReduce;
+import usth.master.common.CallBack;
 import usth.master.common.ClientTransfer;
+import usth.master.worker.Daemon;
 
 /**
  * Communicate to server, execute map and synthesize result.
@@ -23,7 +29,7 @@ public class Launch {
      * */
     public Launch(
         List<Map<String, String>> hosts, Map<String, String> hostFiles,
-        String rootDir) {
+        String rootDir, MapReduce m) {
         this.hosts = hosts;
         this.hostFiles = hostFiles;
         this.rootDir = rootDir;
@@ -43,12 +49,12 @@ public class Launch {
 
         try {
             File rootDirProp = new File(rootDir);
-            if(!rootDirProp.exists()) {
+            if (!rootDirProp.exists()) {
                 rootDirProp.mkdirs();
             }
 
             File localFile = new File(localFileName);
-            if(!localFile.exists()) {
+            if (!localFile.exists()) {
                 localFile.createNewFile();
             }
 
@@ -63,8 +69,40 @@ public class Launch {
     }
 
     /**
-     * Call execute and collect result
+     * Call execute and collect result.
+     * @param m mapreduce executor
+     * @param cb callback for complete information
+     * @return final result file
      * */
-    public Map<String, String> run() {
+    public String run(MapReduce m, CallBack cb) {
+        List<String> localFiles = new ArrayList<String>();
+
+        // execute and collect result
+        for (Map<String, String> host : hosts) {
+            try {
+                // Prepare name for processing
+                String fileId = host.get("host") + ":" + host.get("port");
+                String hostFileName = hostFiles.get(fileId);
+                String hostFileResultName = UUID.randomUUID().toString();
+                String fileName = rootDir + "/" + UUID.randomUUID().toString();
+                String rmiUrl = "rmi://"
+                                + host.get("host") + ":"
+                                + host.get("port") + "/"
+                                + host.get("name");
+
+                // process and get result
+                Daemon worker = (Daemon) Naming.lookup(rmiUrl);
+                worker.call(m, hostFileName, hostFileResultName, cb);
+                if (pullFile(host, hostFileResultName, fileName)) {
+                    localFiles.add(fileName);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String finalResult = rootDir + "/" + UUID.randomUUID().toString();
+        m.executeReduce(localFiles, finalResult);
+        return finalResult;
     }
 }
