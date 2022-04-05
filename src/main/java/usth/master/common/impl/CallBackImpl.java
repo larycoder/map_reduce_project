77a542518e23c1a430a0a73bebb4376cdd2868b4
larycoder <lenhuchuhiep99@gmail.com>
2022-04-05 10:19:34 +0700

    LH    : [fix] avoid notify before wait in callback

--- src/main/java/usth/master/common/impl/CallBackImpl.java
+++ src/main/java/usth/master/common/impl/CallBackImpl.java
@@ -6,18 +6,29 @@ import java.rmi.server.UnicastRemoteObject;
 import usth.master.common.CallBack;
 
 public class CallBackImpl extends UnicastRemoteObject implements CallBack {
-    int nbNode;
+    private Integer nbNode;
+    private Integer nbCplt; // count number of notify
 
     public CallBackImpl(int nbNode) throws RemoteException {
         this.nbNode = nbNode;
+        this.nbCplt = 0;
+    }
+
+    public void set() {
+        nbCplt = 0;
     }
 
     public synchronized void completed() throws RemoteException {
+        nbCplt++;
+        System.out.print("Receive completed notify ");
+        System.out.print("[");
+        System.out.print(nbCplt.toString() + "/" + nbNode.toString());
+        System.out.println("]");
         notify();
     }
 
     public synchronized void waitForAll() {
-        for (int i = 0; i < nbNode; i++) {
+        while (nbCplt < nbNode) {
             try {
                 wait();
             } catch (InterruptedException e) {
